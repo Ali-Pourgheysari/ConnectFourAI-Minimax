c6c9cb022bb4761283e07ab37fd18b05d51481dd
Ali Pourgheysari <a.geysar@gmail.com>
2023-05-25 13:33:28 +0330

    added documentation

--- Code.py
+++ Code.py
@@ -1,15 +1,19 @@
 #import Library 
 from pettingzoo.classic import connect_four_v3
+import numpy as np
+import time
 
 # Create Environment
 env = connect_four_v3.env(render_mode="human")
-
 env.reset()
+
+
 for agent in env.agent_iter():
 
+    time.sleep(1)
     observation, reward, termination, truncation, info = env.last()
 
-    break
+    # break
 
     if termination or truncation:
 
