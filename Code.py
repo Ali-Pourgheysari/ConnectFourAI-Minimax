e36b2b8000f747eae53a883fc62f8ab53b43014d
Ali Pourgheysari <a.geysar@gmail.com>
2023-05-26 12:10:29 +0330

    edited minimax

--- Code.py
+++ Code.py
@@ -7,75 +7,89 @@ import numpy as np
 env = connect_four_v3.env(render_mode="human")
 env.reset()
 
-def heuristic():
-    pass
+def heuristic(observation):
+    
 
 def possible_moves(observation):
     moves = []
     for i in range(7):
-        if observation['action_mask'] == 0:
+        if observation['action_mask'] == 1:
             moves.append(i)
     return moves
 
-def max_value(observation, alpha, beta, depth):
+def minimax(observation, depth, termination, truncation, maximize_player, alpha, betha):
     if depth == 0 or termination or truncation:
         return heuristic(observation)
-    value = float('-inf')
-    for move in possible_moves(observation):
-        new_observation = make_move(observation, move)
-        value = max(value, min_value(new_observation, alpha, beta, depth-1))
-        alpha = max(alpha, value)
-        if beta <= alpha:
+    
+    if maximize_player:
+        value = float('-inf')
+        for move in possible_moves(observation):
+            new_observation = make_move(observation, move, 0)
+            value = max(value, minimax(new_observation, depth-1, termination, truncation, False, alpha, betha))
+            alpha = max(alpha, value)
+            if betha <= alpha:
+                break
+        return value
+    
+    else:
+        value = float('inf')
+        for move in possible_moves(observation):
+            new_observation = make_move(observation, move, 1)
+            value = min(value, minimax(new_observation, depth-1, termination, truncation, True, alpha, betha))
+            betha = min(betha, value)
+            if betha <= alpha:
+                break
+        return value
+    
+def make_move(observation, move, player):
+    new_observation = observation
+    if new_observation['observation'][6, move, :] == 1:
+        new_observation['action_mask'][move] = 0
+
+    for i in range(6):
+        if all(elem == 0 for elem in new_observation[i, move]):
+            new_observation[i, move, player] = 1
             break
-    return value
+        
+    return new_observation
 
-def min_value(observation, alpha, beta, depth):
-    if depth == 0 or termination or truncation:
-        return heuristic(observation)
-    value = float('inf')
-    for move in possible_moves(observation):
-        new_observation = make_move(observation, move)
-        value = min(value, max_value(new_observation, alpha, beta, depth-1))
-        beta = min(beta, value)
-        if beta <= alpha:
-            break
-    return value
-
-def minimax(observation, depth, temp_env):
-    best_score = float('-inf')
-    best_move = None
-    alpha = float('-inf')
-    beta = float('inf')
-
-    for move in possible_moves(observation):
-        new_observation = make_move(observation, move)
-        score = min_value(new_observation, alpha, beta, depth-1)
-        if score > best_score:
-            best_score = score
-            best_move = move
-        alpha = max(alpha, score)
-    return best_move
-
-counter = 0
 for agent in env.agent_iter():
-    counter += 1
 
-    observation, reward, termination, truncation, info = env.last()
+    observation, _, termination, truncation, info = env.last()
 
     if termination or truncation:
-
         action = None
 
     else:
-        
-        minimax(observation, 3, )
-
-        # Retrun action
-        action = env.action_space(agent).sample()  # this is where you would insert your policy
-
-    if agent == 'player_1':
-        action = int(input("Enter your action(0-6): "))
+        if agent == 'player_0':
+            action = minimax(observation, 3, termination, truncation, True, float('-inf'), float('inf'))
+        else:
+            action = int(input("Enter your action(0-6): "))
 
     env.step(action)
 
 env.close()
+
+# def max_value(observation, alpha, beta, depth, termination, truncation):
+#     if depth == 0 or termination or truncation:
+#         return heuristic(observation)
+#     value = float('-inf')
+#     for move in possible_moves(observation):
+#         new_observation = make_move(observation, move)
+#         value = max(value, min_value(new_observation, alpha, beta, depth-1))
+#         alpha = max(alpha, value)
+#         if beta <= alpha:
+#             break
+#     return value
+
+# def min_value(observation, alpha, beta, depth, termination, truncation):
+#     if depth == 0 or termination or truncation:
+#         return heuristic(observation)
+#     value = float('inf')
+#     for move in possible_moves(observation):
+#         new_observation = make_move(observation, move)
+#         value = min(value, max_value(new_observation, alpha, beta, depth-1))
+#         beta = min(beta, value)
+#         if beta <= alpha:
+#             break
+#     return value
